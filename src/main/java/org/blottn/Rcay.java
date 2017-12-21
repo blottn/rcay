5b25cb4cd14e1d216c25d145118055a8b6e0b153
Nicholas Blott <blottn@tcd.ie>
2017-12-21 18:10:07 +0000

    added motion

--- src/main/java/org/blottn/Rcay.java
+++ src/main/java/org/blottn/Rcay.java
@@ -7,6 +7,8 @@ import java.awt.Color;
 import java.util.*;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Rcay {
 
@@ -19,29 +21,75 @@ public class Rcay {
 		static final double fov = 90;
 
 	public static void main(String[] args) throws Exception {
+
+		double MOVEMENT_SPEED = 0.2;
+
 		String title = "Rcay";
 
 		ArrayList<Entity> ents = new ArrayList<Entity>();
-		ents.add(new Entity(50,10));
+		ents.add(new Entity(95,10));
+		ents.add(new Entity(50,0));
+		ents.add(new Entity(10,-30));
+		ents.add(new Entity(20,-50));
 		ents.add(new Entity(50,-10));
-		ents.add(new Entity(5,0));
+		ents.add(new Entity(70,-25));
+		ents.add(new Entity(10,0));
+		ents.add(new Entity(22,-20));
 
 
 		final World world = new World(ents);
+
+		Listener listener = new Listener(world);
+
 		world.setPos(0,0);
 
 		JFrame frame = new JFrame(title);
 		frame.setSize(width,height);
 		frame.show();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(listener);
+		double[] movement;
 		while (true) {
 			try {
 				Thread.sleep(5);
-			} catch(Exception e) {
-
+			} catch(Exception e) {}
+			movement = new double[]{0,0};
+			for (KeyEvent e : listener.inputs.keySet()) {
+				if (e.getID() == KeyEvent.KEY_PRESSED) {
+					if (e.getKeyChar() == 'w') {
+						movement[0] += MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 's') {
+						movement[0] -= MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 'a') {
+						movement[1] -= MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 'd') {
+						movement[1] += MOVEMENT_SPEED;
+					}
+				}
+				else if (e.getID() == KeyEvent.KEY_RELEASED) {
+					if (e.getKeyChar() == 'w') {
+						movement[0] -= MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 's') {
+						movement[0] += MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 'a') {
+						movement[1] += MOVEMENT_SPEED;
+					}
+					else if (e.getKeyChar() == 'd') {
+						movement[1] -= MOVEMENT_SPEED;
+					}
+				}
 			}
+			
+			world.xPos += movement[0];
+			world.yPos += movement[1];
+
+			listener.inputs.clear();
 			render(frame.getGraphics(),world);
-			world.yPos = world.yPos + 0.05;
 		}
 	}
 
@@ -149,5 +197,30 @@ public class Rcay {
 			posX = x;
 			posY = y;
 		}
+	}
+
+	public static class Listener implements KeyListener {
+		World world;
+
+		public HashMap<KeyEvent,Integer> inputs;
+
+		public Listener(World world) {
+			inputs = new HashMap<KeyEvent, Integer>();
+			this.world = world;
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			inputs.put(e,null);
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			inputs.put(e,null);
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}	//unused
+
 	}
 }
